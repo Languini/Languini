ee3a22e3c82871ab35e7e1e487fda2db1e77bb79
Eleanor Mark <emark@calvisitor-10-105-156-128.calvisitor.1918.berkeley.edu>
2017-05-13 14:23:14 -0700

    added models :-)

---
+++ app/server/models/comment.js
@@ -0,0 +1,19 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+    const Comment = sequelize.define('Comment', {
+        content: {
+            type: DataTypes.STRING,
+            validate: {
+                notNull: true
+            }
+        }
+    }, {
+        classMethods: {
+            associate: models => {
+                Comment.belongsTo(models.Answer)
+                Comment.belongsTo(models.User)
+            }
+        }
+    })
+    return Comment
+}
