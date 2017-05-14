1f0183ea0cd15a00073fd46c7bd3ecdd3a5748c7
CodingKaiser <falkonoe@live.com>
2017-05-14 15:49:15 -0700

    Simple call to api established on create html page submission.

--- app/server/controllers/api.controller.js
+++ app/server/controllers/api.controller.js
@@ -4,4 +4,5 @@ exports.someFunction = (req, res) => {
 
 exports.createPost = (req, res) => {
   console.log(req.body);
+  res.json(req.body);
 }
--- app/src/create.js
+++ app/src/create.js
@@ -1,8 +1,15 @@
 // this is going to hold the js for the post creation page
 module.exports = {
   listenForSubmission: () => {
-    $("#submit-post").on("click", () => {
-      console.log("Hello World!");
+    $("#submit-post").on("click", (event) => {
+      event.preventDefault()
+      var translation = {
+        request: $("#request").val().trim(),
+        context: $("#context").val().trim()
+      }
+      $.post("/api/create", translation, () => {
+        console.log("Submitted a new post");
+      });
     })
   }
 }
