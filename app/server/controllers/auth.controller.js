74690519fad133c238486fdb29dfa9b69f9d738b
ycleptkellan <yclepkellan@gmail.com>
2017-05-14 18:09:46 -0700

    <\                     __  \\                 .~~  ~~.   \\               /|~|     |    \\             /======\  |   //\\           |>/_\_<_=

--- app/server/controllers/auth.controller.js
+++ app/server/controllers/auth.controller.js
@@ -4,22 +4,21 @@ const passport = require('passport'),
   FacebookStrategy = require('passport-facebook').Strategy,
   { User } = require('../models')
 
-
 passport.use(new FacebookStrategy({
-    clientID: process.env.APP_ID,
-    clientSecret: process.env.APP_SECRET,
-    callbackURL: 'http://localhost:5000/auth/facebook/callback',
-    profileFields: ['id','displayName', 'photos', 'emails']
-  }, (accessToken, refreshToken, profile, done) => {
+  clientID: process.env.APP_ID,
+  clientSecret: process.env.APP_SECRET,
+  callbackURL: 'http://localhost:5000/auth/facebook/callback',
+  profileFields: ['id', 'displayName', 'photos', 'emails']
+}, (accessToken, refreshToken, profile, done) => {
     /* should be findOrCreate */
-    User
+  User
       .create({
         fb_id: profile.id,
         email: profile.emails[0].value,
         name: profile.displayName,
         photo: profile.photos[0].value
       })
-  }
+}
 ))
 
 passport.serializeUser((user, done) => {
--- app/server/models/answer.js
+++ app/server/models/answer.js
@@ -1,20 +1,28 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-    const Answer = sequelize.define('Answer', {
-        content: {
-            type: DataTypes.STRING,
-            validate: {
-                notNull: true
-            }
-        }
-    }, {
-        classMethods: {
-            associate: models => {
-                Answer.belongsTo(models.Translation)
-                Answer.belongsTo(models.User)
-                Answer.hasMany(models.Comment)
-            }
-        }
-    })
-    return Answer
+  const Answer = sequelize.define('Answer', {
+    content: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [2, 500],
+        notNull: true
+      }
+    },
+    votes: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: true,
+        isInt: true,
+      }
+    }
+  }, {
+    classMethods: {
+      associate: models => {
+        Answer.belongsTo(models.Translation)
+        Answer.belongsTo(models.User)
+        Answer.hasMany(models.Comment)
+      }
+    }
+  })
+  return Answer
 }
