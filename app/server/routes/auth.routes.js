a4b0427d8e4cc462694106e0459078594aebca98
ycleptkellan <yclepkellan@gmail.com>
2017-05-15 22:21:32 -0700

    ▂▃▅▇█▓▒░۩۞۩        ۩۞۩░▒▓█▇▅▃▂

---
+++ app/server/routes/auth.routes.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+  router = express.Router(),
+  authController = require('../controllers/auth.controller')
+
+module.exports = (app, passport) => {
+  router
+    .get('/facebook',
+      passport.authenticate(
+        'facebook',
+        { scope: 'email'})
+      )
+    .get('/facebook/callback',
+      passport.authenticate(
+        'facebook',
+        { failureRedirect: '/' }
+      ),
+      authController.loggedIn)
+    .get('/logout', authController.logout)
+
+  app.use('/auth', router)
+}
